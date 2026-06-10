d54ad1a36ba5c677fc40b01b5e2ad0b1fee4ca96
KevinAburto <kevinaburtomuela@hotmail.com>
2026-06-09 18:52:06 -0600

    Add cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,17 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
     baseUrl: 'https://the-internet.herokuapp.com',
+
+    retries: {
+      runMode: 0,   // cypress run
+      openMode: 0   // cypress open
+    },
+
     env: {
-    username: 'admin',
-    password: 'admin'
-}
-  },
+      username: 'admin',
+      password: 'admin'
+    }
+  }
 });
